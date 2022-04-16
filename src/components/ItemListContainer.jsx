85d912e66b5f5934b57ff463d02697dabc815327
Pablo Arias <pabloarias.ic@gmail.com>
2022-04-16 14:20:50 -0400

    ItemListContainer props added

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function ItemListContainer() {
+function ItemListContainer(props) {
   return (
     <div>
       <h1>Listado de productos</h1>
-      <p>Pronto</p>
+      <p>{props.text}</p>
     </div>
   );
 }
