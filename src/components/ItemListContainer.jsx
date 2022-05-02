ab3e2cc5fc7c7971ee92fcec0047d234d91272fe
Pablo Arias <pabloarias.ic@gmail.com>
2022-05-01 22:54:43 -0400

    Desafio clase 5

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
-import ItemCard from './ItemCard';
+import React, { useEffect, useState } from "react";
+import ItemCard from "./ItemCard";
+import ItemList from "./ItemList";
 
 function ItemListContainer(props) {
+  const onAddFn = () => console.log("onAdd test");
+  const itemData = [
+    {
+      title: "Bicicleta Aro 29",
+      image: "fenixaro29.jpg",
+      price: 22000,
+      initial: 3,
+      stock: 6,
+      onAdd: onAddFn,
+    },
+    {
+      title: "Moto Yamaha",
+      image: "yamahamoto.jpg",
+      price: 522000,
+      initial: 2,
+      stock: 5,
+      onAdd: onAddFn,
+    },
+    {
+      title: "Auto BMW Ces2018",
+      image: "bmwces2018auto.jpg",
+      price: 692000,
+      initial: 0,
+      stock: 2,
+      onAdd: onAddFn,
+    },
+  ];
+
+  const [itemCardData,setItemCardData] = useState([]);
+
+  useEffect(() => {
+    const loadData = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(itemData);
+      }, 2000);
+    });
+    loadData
+      .then((res) => setItemCardData(res))
+      .catch((err) => console.log("Error: ", err));
+    return () => {};
+  }, []);
+
   return (
     <div>
       <h1>Listado de productos</h1>
-      <ItemCard title="Bicicleta Fenix Aro 29" image="fenixaro29.jpg" price="109.990" initial="1" stock="3" onAdd={(n) => console.log(`Cantidad a agregar ${n}`)}/>
+      <ItemList data={itemCardData} />
     </div>
   );
 }
